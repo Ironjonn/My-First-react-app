c0f2d659a1a876df0603ae024a3442bfb3bab4b7
Jonathan Franco <jonfranco.9212@gmail.com>
2020-02-13 22:30:32 -0600

    se agrego un nuevo formulario de bootstrap

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -27,16 +27,41 @@ const Formulario = () => {
     }
 
     return (
+
+        <div>
         <div>
             <form className="container-sm mt-5" onSubmit={enviarDatos}>
                 <input type="text" placeholder=" Ingrese su Usuario" className="form-control" name="usuario" onChange={SetData} ></input>
                 <input type="text" placeholder="Ingrese su contraseña" className="form-control" name="contraseña" onChange={SetData}  ></input>
-                <button className="btn btn-success" type="submit">Continuar</button>
+                <button className="btn btn-primary mt-3" type="submit">Continuar</button>
             </form>
             <div> {datos.usuario} </div>
             <div> {datos.contraseña} </div>
+
         </div>
-    );
+
+        <div>    
+        <form>
+            <div className="form-group">
+                <label for="exampleInputEmail1">Email address</label>
+                <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"> </input>
+                <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+            </div>
+            <div className="form-group">
+                <label for="exampleInputPassword1">Password</label>
+                <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password"> </input>
+            </div>
+            <div className="form-check">
+                <input type="checkbox" className="form-check-input" id="exampleCheck1"> </input>
+                <label className="form-check-label" for="exampleCheck1">Check me out</label>
+            </div>
+            <button type="submit" className="btn btn-primary">Submit</button>
+        </form>
+        </div>
+        </div>
+     
+
+         );
 
 }
 
